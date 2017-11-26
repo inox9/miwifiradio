a0b867beaa282317d3cff2939c7cadddb7309799
inox <inox@noc.od.ua>
2017-11-27 00:22:13 +0200

    fixed ffkill interpreter

--- ffkill.php
+++ ffkill.php
@@ -8,17 +8,16 @@ include_once($global_path."include/function.php");
 error_reporting(0);
 
 $result = $db->sql_query("SELECT pid,title,xid, UNIX_TIMESTAMP(lastrequest) FROM radios where lastrequest is not NULL;");
-while ( list($pid, $title, $xid, $lastrequest)= $db->sql_fetchrow($result) ) {
-    $delta=time()-$lastrequest;
-    // If radio does not listenning more than 120 sec, then kill pid
-    if(intval(time()-$lastrequest)>120) {
-	// Kill PID
-        exec("kill $pid");
-	sleep(1);
-        // Now let's remove the playing dir
-        rrmdir($global_path . 'uploads/playing'.$xid);
-        // Updating table radios
-        $db->sql_query("UPDATE radios SET pid=NULL,lastrequest=NULL WHERE xid='$xid'");
-//        echo "ffmpeg($pid) playing ($title) was killed by timeout\n";
-    }
+while (list($pid, $title, $xid, $lastrequest) = $db->sql_fetchrow($result)) {
+	$delta = time() - $lastrequest;
+	// If radio does not listenning more than 120 sec, then kill pid
+	if (intval(time() - $lastrequest) > 120) {
+		// Kill PID
+		exec("kill $pid");
+		sleep(1);
+		// Now let's remove the playing dir
+		rrmdir($global_path . 'uploads/playing' . $xid);
+		// Updating table radios
+		$db->sql_query("UPDATE radios SET pid=NULL,lastrequest=NULL WHERE xid='$xid'");
+	}
 }
